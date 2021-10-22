4e8e45f29828c54aa8743aeecc3756adfed6b79a
kawabata <s1931115@s.konan-u.ac.jp>
2021-10-22 19:56:10 +0900

    [UPDATE]TypingGameのHeaderとスタート画面の作成とTypingGameのロジックの組み立て途中

---
+++ src/components/common/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-gray-700 h-10 flex items-center">
+    <div class="pl-5 flex gap-1.5">
+      <button type="button" class="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm  bg-red-600"></button>
+      <button type="button" class="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm  bg-gray-600"></button>
+      <button type="button" class="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm  bg-green-600"></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Header'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
+import HelloWorld from '@/components/HelloWorld.vue';
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class=" bg-black h-screen text-white text-sm ">
+      <div class="text-white">
+        <span class="text-green-500">Typinguser@TypingusernoMacBookMax</span>
+        <span>:</span>
+        <span class="text-blue-500 px-2">~</span><br />
+        <div class="flex">
+          <label for="comand" class="w-4">$</label>
+          <input
+            type="text"
+            class="bg-black flex-1"
+            v-model="inputCommandRef"
+            @keydown.enter="Useranswer"
+            autocomplete="off"
+            id="comand"
+          />
+        </div>
+        <p v-show="!collectCommand" class="text-red-500">Error:Please type 'cd TypingGame'</p>
+        <p>
+          Hello World !<br />
+          If you want to start the game , You type 'cd TypingGame' after $ and
+          press enter.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { ref } from "@vue/reactivity";
+import {useRouter} from 'vue-router'
+// import {reactive} from 'vue'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  setup() {
+    const router=useRouter()
+    const inputCommandRef = ref("");
+    const collectCommand =ref(true)
+    const Useranswer = () => {
+      console.log(inputCommandRef.value);
+      if (inputCommandRef.value === "cd TypingGame") {
+        console.log("gamestart");
+        router.push('/game')
+      } else {
+        console.log("gamenostart");
+        collectCommand.value=false
+      }
+    };
+    return {
+      Useranswer,
+      inputCommandRef,
+      collectCommand
+    };
+  },
+};
 </script>
